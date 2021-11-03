52a894f4d078152b1c932e412a8cf909c0f342bb
Alan Morgan <alanxoc3@gmail.com>
2021-11-03 14:40:01 -0600

    beg script & end script

--- lookatme/contrib/script.py
+++ lookatme/contrib/script.py
@@ -11,15 +11,22 @@ import lookatme.render
 from lookatme.exceptions import IgnoredByContrib
 import lookatme.config
 
-SCRIPTS = {}
+BEG_SCRIPTS = {}
+END_SCRIPTS = {}
 def transition_slide(next_slide, prev_slide):
-    if next_slide > prev_slide and next_slide in SCRIPTS:
-        subprocess.run(SCRIPTS[next_slide])
+    if next_slide > prev_slide:
+        if prev_slide in END_SCRIPTS:
+            subprocess.run(END_SCRIPTS[prev_slide])
+            del END_SCRIPTS[prev_slide]
+        if next_slide in BEG_SCRIPTS:
+            subprocess.run(BEG_SCRIPTS[next_slide])
+            del BEG_SCRIPTS[next_slide]
 
 def render_code(token, body, stack, loop):
     lang = token["lang"] or ""
     slide = token["slide"] or 0
-    if lang == 'script': SCRIPTS[slide] = shlex.split(token["text"])
+    if   lang == 'beg-script': BEG_SCRIPTS[slide] = shlex.split(token["text"])
+    elif lang == 'end-script': END_SCRIPTS[slide] = shlex.split(token["text"])
     else: raise IgnoredByContrib
 
     return []
